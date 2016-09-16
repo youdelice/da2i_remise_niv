992bcdadd3c998ddced50cb9ead88c73bbdd71ec
Clement LAMBERT <lambertc@meleze25.iut-infobio.priv.univ-lille1.fr>
2016-09-16 11:13:15 +0200

    test error

--- da2i_remise_niv/src/da2i_remise_niv/Da2i_remise_niv.java
+++ da2i_remise_niv/src/da2i_remise_niv/Da2i_remise_niv.java
@@ -17,7 +17,7 @@ public class Da2i_remise_niv {
      */
     public static void main(String[] args) {
 
-        System.out.println("antoine2");
+        System.out.println("antoine");
     }
     
 }
