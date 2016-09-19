cc575280d844d6c59a325332bbf252a090609739
Jeffrey MARKEY <markeyj@meleze24.iut-infobio.priv.univ-lille1.fr>
2016-09-19 16:43:38 +0200

    detection tout alien

--- da2i_remise_niv/src/da2i_remise_niv/MouvementProjectile.java
+++ da2i_remise_niv/src/da2i_remise_niv/MouvementProjectile.java
@@ -13,23 +13,25 @@ public class MouvementProjectile extends Thread {
         this.game = game;
     }
 
-    public void run() 
-    {
+    public void run() {
         Point p = projectile.getLocation();
         p.y -= 15;
 
-        while (projectile.getLocation().y > 10) 
-        {
+        while (projectile.getLocation().y > 10) {
             p = projectile.getLocation();
 
             p.y -= 10;
 
             projectile.setLocation(p);
-            
-            for(List<Alien> alien : game.colonneAlien)
-            {
-                if(!alien.isEmpty())
-                   colision(alien, alien.get(alien.size() - 1 ).getX(), alien.get(alien.size() - 1 ).getY());
+
+            for (List<Alien> aliens : game.colonneAlien) {
+                if (!aliens.isEmpty()) {
+                    for (Alien alien : aliens) {
+                           colision(aliens, alien.getX(), alien.getY(), alien);
+                    }
+                }
+
+                
             }
 
             try {
@@ -38,32 +40,28 @@ public class MouvementProjectile extends Thread {
                 e.printStackTrace();
             }
         }
-            projectile.removeProjectile();
+        projectile.removeProjectile();
     }
 
-    public void colision(List<Alien> aliens, int coX, int coY) 
-    {
-        if ((projectile.getY() >= coY) && (projectile.getY() <= coY + 40)) 
-        {
+    public void colision(List<Alien> aliens, int coX, int coY, Alien alien) {
+        if ((projectile.getY() >= coY) && (projectile.getY() <= coY + 40)) {
             if ((projectile.getX() >= coX) && (projectile.getX() <= coX + 40)) {
                 System.out.println("touché ");
 
-                this.game.getPanelGame().remove(aliens.get(aliens.size() - 1));
-                this.game.aliensLigne.remove(aliens.get(aliens.size() - 1));
-
-                aliens.remove(aliens.size() - 1);
+                this.game.getPanelGame().remove(alien);
+                this.game.aliensLigne.remove(alien);
+                aliens.remove(alien);
+                
                 this.projectile.removeProjectile();
 
-                try 
-                {
-                    this.game.setScore(this.game.getScore()+5);
-                    this.game.lb_score.setText("Score : " + this.game.getScore()); 
+                try {
+                    this.game.setScore(this.game.getScore() + 5);
+                    this.game.lb_score.setText("Score : " + this.game.getScore());
                     System.out.println(this.game.getScore());
                     projectile = null;
                     this.join();
-                    
-                } catch (Exception e) {
 
+                } catch (Exception e) {
                 }
             }
         }
