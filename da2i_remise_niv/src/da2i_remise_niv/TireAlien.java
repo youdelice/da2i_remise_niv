dd7fd3ba7ea642d121fcadbf9cf5d2a4224209d0
Jeffrey MARKEY <markeyj@meleze23.iut-infobio.priv.univ-lille1.fr>
2016-09-20 15:57:19 +0200

    blablabla2

--- da2i_remise_niv/src/da2i_remise_niv/TireAlien.java
+++ da2i_remise_niv/src/da2i_remise_niv/TireAlien.java
@@ -34,9 +34,9 @@ public class TireAlien extends Thread {
                             System.out.println("TireAlien task : " + e.getMessage());
                         }
                     }
-                }, rand.nextInt(2500));
+                }, rand.nextInt(1250));
 
-                Thread.sleep(1250);
+                Thread.sleep(250);
             } catch (Exception e) 
             {
                    System.out.println("TireAlien : " + e.getMessage());
