85baf88b796e2f6edc8edcdc46acc4bb5f23ab44
Cyril SANT <santc@meleze24.iut-infobio.priv.univ-lille1.fr>
2016-09-21 15:25:23 +0200

    score2

--- da2i_remise_niv/src/da2i_remise_niv/ScoreJeu.java
+++ da2i_remise_niv/src/da2i_remise_niv/ScoreJeu.java
@@ -9,10 +9,15 @@ package da2i_remise_niv;
 import bdd.Bdd;
 import bdd.Score;
 import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.util.List;
 import javax.swing.JLabel;
+import javax.swing.DefaultListModel;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
@@ -22,6 +27,9 @@ import javax.swing.ListSelectionModel;
  */
 public class ScoreJeu extends JFrame {
     
+    private Bdd dd;
+    private List<String> liste;
+    private List<String> listScore;
     JPanel jp;
     JLabel jl;
     JList list, list2;
@@ -37,32 +45,69 @@ public class ScoreJeu extends JFrame {
          affichScore();
     }
     
-    public void affichScore() {
+    public void affichScore() { // ((DefaultListModel)list2.getModel()).addElement(String.valueOf(liste.get(i).getScore()));
         try{
-            Bdd dd = new Bdd();
-
-            List<Score> liste = dd.getAllScore();
+            dd = new Bdd();
+            
+             liste = dd.getAllScore();
             System.out.println(liste.size());
             String[] nomJoueur = new String[liste.size()];
             String[] scoreJoueur = new String[liste.size()];
-            System.out.println(liste.get(0).getScore());
+            
+            
             
             for(int i = 0; i<nomJoueur.length; i++)
-                nomJoueur[i] = liste.get(i).getPseudo();
+                nomJoueur[i] = liste.get(i);
             list = new JList(nomJoueur);
             list.setSize(200,500);
             list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
             list.setLayoutOrientation(JList.VERTICAL);
             list.setVisibleRowCount(-1);
+            list.addMouseListener(new MouseListener() {
+
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    DefaultListModel listmodel = (DefaultListModel)list2.getModel();
+                    listmodel.removeAllElements();
+                    int a = list.getSelectedIndex();
+                    String pseudo = liste.get(a);
+                    try{
+                        listScore = dd.getScore(pseudo);
+                        for(String score : listScore)
+                            ((DefaultListModel)list2.getModel()).addElement(score);
+                    }catch(Exception z) {z.printStackTrace();}
+                   // list2.remo
+                    
+                    
+                }
+
+                @Override
+                public void mousePressed(MouseEvent e) {
+                   // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+                   // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
+
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                   //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+                   // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
+            });
             
-            JScrollPane listScroller = new JScrollPane(list);
-            listScroller.setSize(200, 500);
-            listScroller.setLocation(0, 0);
-            this.getContentPane().add(listScroller);
+           
             
-            for(int i = 0; i<scoreJoueur.length; i++)
-                scoreJoueur[i] = String.valueOf(liste.get(i).getScore());
-            list2 = new JList(scoreJoueur);
+            list2 = new JList();
+            DefaultListModel model = new DefaultListModel();
+            list2.setModel(model);                
+            
             list2.setSize(200,500);
             list2.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
             list2.setLayoutOrientation(JList.VERTICAL);
@@ -73,10 +118,13 @@ public class ScoreJeu extends JFrame {
             listScroller2.setLocation(200, 0);
             this.getContentPane().add(listScroller2);
             
-            
+            JScrollPane listScroller = new JScrollPane(list);
+            listScroller.setSize(200, 500);
+            listScroller.setLocation(0, 0);
+            this.getContentPane().add(listScroller);   
         }
         catch (Exception e){
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 }
